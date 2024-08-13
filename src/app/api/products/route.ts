bd88ef643932461fd31a7ab3381e8874e809ed78
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-08-13 23:32:04 +0530

    Product insertion endpoint created

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,40 @@
+import { productSchema } from "@/lib/validators/productSchema";
+import { writeFile } from "node:fs/promises";
+import { NextRequest, NextResponse } from "next/server";
+import path from "node:path";
+import { db } from "@/lib/db/db";
+import { products } from "@/lib/db/schema";
+
+export async function POST(req: NextRequest) {
+    const data = await req.formData();
+
+    let validatedData;
+
+    try {
+        validatedData = productSchema.parse({
+            name: data.get("name"),
+            image: data.get("image"),
+            description: data.get("description"),
+            price: Number(data.get("price")),
+        });
+    } catch (error) {
+        return NextResponse.json({status: 400, message: error});
+    }
+
+    const filename = `${Date.now()}.${validatedData.image.name.split(".").slice(-1)}`;
+
+    try {
+        const buffer = Buffer.from(await validatedData.image.arrayBuffer());
+        await writeFile(path.join(process.cwd(), "public/assets", filename), buffer);
+    } catch (error) {
+        return NextResponse.json({status: 500, message: "Failed to save the file to filesystem"})
+    }
+
+    try {
+        await db.insert(products).values({...validatedData, image: filename});
+    } catch (error) {
+        return NextResponse.json({status: 500, message: "Failed to store product into the database"});
+    }
+
+    return NextResponse.json({status: 201, message: "OK! everything went well"});
+}
